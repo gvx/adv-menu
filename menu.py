34532a9a0e77864a41fe6b819c7efb76f422de01
gvx <gyvox.public@gmail.com>
2009-01-23 20:18:48 +0100

    Version 1.1.1

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-#ADV-MENU 1.1
+#ADV-MENU 1.1.1
 #(C) 2008 Robin Wellner
 #
 #This program is free software: you can redistribute it and/or modify
@@ -151,29 +151,3 @@ def menu(Surface, Items, Xoffset, Xoffset2, Yoffset, itemheight, totalheight, bo
                     Surface.blit(Font.render(draw_item, True, (255, 255, 255)),
                                  (Xoffset+15, Yoffset+ 5 + n*totalheight))
         pygame.display.flip()
-
-
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption("ADV-MENU Test")
-    icon = pygame.Surface((1,1)); icon.set_alpha(0); pygame.display.set_icon(icon)
-    Surface = pygame.display.set_mode((640,480))#, SCR_FULL and FULLSCREEN)
-    Font = pygame.font.Font("mksanstallx.ttf",14)
-    Items = [('Abc', 'abc', 'button'),
-             ('Do something', 'x', 'slider', (2, 0, 10)),
-             ('Done', 'p', 'checkbox', True),
-             ('Test', 'name', 'disabled'),
-             ('Quit', 'exit', 'button'),
-            ]
-    result = menu(Surface, Items, 30, 200, 30, 30, 50, 300, Font)
-    if result[0] in ('cancel', 'exit'):
-        print "User quitted"
-    elif result[0] == 'abc':
-        print "User chose Abc"
-    elif result[0] == 'name':
-        print "User chose Test"
-    print "Slider index:",
-    print result[1]['x'].index
-    print "Checkbox checked:",
-    print result[1]['p'].checked
-    pygame.quit()
---
+++ menu_test.py
@@ -0,0 +1,36 @@
+#ADV-MENU TEST 1.0
+#(C) 2008 Robin Wellner
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#Released under GPL (see http://www.gnu.org/licenses/)
+
+from menu import menu, pygame
+pygame.init()
+pygame.display.set_caption("ADV-MENU Test")
+icon = pygame.Surface((1,1)); icon.set_alpha(0); pygame.display.set_icon(icon)
+Surface = pygame.display.set_mode((640,480))
+
+Font = pygame.font.Font("mksanstallx.ttf",14)
+
+Items = [('Abc', 'abc', 'button'),
+         ('Do something', 'x', 'slider', (2, 0, 10)),
+         ('Done', 'p', 'checkbox', True),
+         ('Test', 'name', 'disabled'),
+         ('Quit', 'exit', 'button'),
+        ]
+result = menu(Surface, Items, 30, 200, 30, 30, 50, 300, Font)
+
+if result[0] in ('cancel', 'exit'):
+    print "User quitted"
+elif result[0] == 'abc':
+    print "User chose Abc"
+elif result[0] == 'name':
+    print "User chose Test"
+print "Slider index:",
+print result[1]['x'].index
+print "Checkbox checked:",
+print result[1]['p'].checked
+pygame.quit()
